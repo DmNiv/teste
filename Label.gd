864ddb99f554da9a95dead57c1eaf401f2115b0b
Pedro Castro <pedro7mirand@gmail.com>
2023-10-05 13:10:44 -0300

    atualizações na entrada e saída de texto

--- Label.gd
+++ Label.gd
@@ -1,5 +1,16 @@
 extends Label
 
+var textoRecebido: String
+
+func mostrarTexto():
+	visible_ratio = lerp(1, 0, get_node("Timer").time_left / get_node("Timer").wait_time)
+
+func recebeTexto(texto: String):
+	textoRecebido = texto
+	text = "Olá {nome}, prazer em conhecer!".format({"nome": textoRecebido})
+	visible_ratio = 0
+	get_node("Timer").start(get_node("Timer").wait_time)
+	mostrarTexto()
 
 func _process(delta):
-	visible_ratio = lerp(1, 0, get_node("Timer").time_left / get_node("Timer").wait_time)
+	mostrarTexto()
--- LineEdit.gd
+++ LineEdit.gd
@@ -1,12 +1,15 @@
 extends LineEdit
 
+var valorInserido
 
-
+func mandaTexto(texto: String,label: Node):
+	label.recebeTexto(texto)
 
 func _ready():
 	grab_focus()
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_enter"):
-		print(text)
+		valorInserido = text
+		mandaTexto(valorInserido, get_parent().get_node("Label"))
 		clear()
